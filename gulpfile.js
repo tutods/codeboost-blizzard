111bfd0962d85330544631dd16b5c198576f3937
Daniel Sousa @TutoDS <jdaniel.asousa@gmail.com>
2022-07-29 21:46:33 +0100

    feat: gulp base template

---
+++ gulpfile.js
@@ -0,0 +1,103 @@
+import gulp from 'gulp';
+import dartSass from 'sass';
+import gulpSass from 'gulp-sass';
+import gulpAutoprefixer from 'gulp-autoprefixer';
+import gulpConcat from 'gulp-concat';
+import gulpBabel from 'gulp-babel';
+import gulpUglify from 'gulp-uglify';
+import browserSync from 'browser-sync';
+
+const sass = gulpSass(dartSass);
+
+browserSync.create();
+
+// Build all scss files into css files and add autoprefixer
+const buildStyles = () => {
+	return gulp
+		.src('./scss/**/*.scss')
+		.pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+		.pipe(
+			gulpAutoprefixer({
+				overrideBrowserslist: ['last 2 versions'],
+				cascade: false
+			})
+		)
+		.pipe(gulp.dest('./css'))
+		.pipe(browserSync.stream());
+};
+
+// Init Browser Sync
+const browserSyncInit = () => {
+	browserSync.init({
+		server: {
+			baseDir: './'
+		}
+	});
+};
+
+// Build Javascript code
+const buildJS = () => {
+	return gulp
+		.src('./js/scripts/**/*.js')
+		.pipe(gulpConcat('app.min.js'))
+		.pipe(
+			gulpBabel({
+				presets: ['@babel/env']
+			})
+		)
+		.pipe(gulpUglify())
+		.pipe(gulp.dest('./js/'))
+		.pipe(browserSync.stream());
+};
+
+const buildPackagesJS = () => {
+	return gulp
+		.src(['./js/libs/swiper.min.js', './js/libs/aos.min.js'], { allowEmpty: true })
+		.pipe(gulpConcat('packages.min.js'))
+		.pipe(
+			gulpBabel({
+				presets: ['@babel/env']
+			})
+		)
+		.pipe(gulpUglify())
+		.pipe(gulp.dest('./js/'))
+		.pipe(browserSync.stream());
+};
+
+const buildPackagesStyles = () => {
+	return gulp
+		.src(['./css/libs/swiper.min.css', './css/libs/aos.min.css'], { allowEmpty: true })
+		.pipe(gulpConcat('packages.min.css'))
+		.pipe(gulp.dest('./css/'))
+		.pipe(browserSync.stream());
+};
+
+// Tasks
+gulp.task('sass', buildStyles);
+
+gulp.task('browser-sync', browserSyncInit);
+
+gulp.task('js', buildJS);
+
+gulp.task('packages:js', buildPackagesJS);
+gulp.task('packages:css', buildPackagesStyles);
+
+// Watch for changes
+const watch = () => {
+	gulp.watch('./scss/**/*.scss', gulp.series('sass'));
+
+	gulp.watch('./**/*.html').on('change', browserSync.reload);
+
+	gulp.watch('./js/scripts/*.js', gulp.series('js'));
+
+	gulp.watch('./css/libs/*.css', gulp.series('packages:css'));
+	gulp.watch('./js/libs/*.js', gulp.series('packages:js'));
+};
+
+gulp.task('watch', watch);
+
+// Default task
+gulp.task(
+	'default',
+	gulp.parallel('watch', 'browser-sync', 'sass', 'js', 'packages:js', 'packages:css')
+);
